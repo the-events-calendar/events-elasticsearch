bec4ac6569c125c6d30cdc925565d904caf69231
Scott Kingsley Clark <lol@scottkclark.com>
2017-07-20 01:37:28 -0500

    More hooks for ElasticPress / Elasticsearch

--- src/Tribe/ElasticPress.php
+++ src/Tribe/ElasticPress.php
@@ -62,13 +62,19 @@ class Tribe__Events__Elasticsearch__ElasticPress {
 		// Whitelist meta keys
 		add_filter( 'ep_prepare_meta_whitelist_key', array( $this, 'whitelist_meta_keys' ), 10, 3 );
 
+		// Add ElasticPress integration for TEC queries
+		add_action( 'tribe_events_pre_get_posts', array( $this, 'add_ep_query_integration' ), 9 );
+
+		// Remove TEC wpdb overrides for WP_Query
+		add_action( 'tribe_events_pre_get_posts', array( $this, 'remove_tec_wpdb_overrides' ) );
+
+		// Override ElasticPress post_date used with _EventStartDate
+		add_filter( 'ep_post_sync_args', array( $this, 'override_ep_post_date' ), 10, 2 );
+
 	}
 
 	/**
 	 * Handle auto integration of ElasticPress for TEC post types.
-	 *
-	 * @todo Add option for auto integration for all queries, default on
-	 * @todo Add option for auto integration for admin queries, default off
 	 *
 	 * @param WP_Query $query
 	 */
@@ -79,15 +85,15 @@ class Tribe__Events__Elasticsearch__ElasticPress {
 			return;
 		}
 
-		/**
-		 * Enable admin integration for TEC post types.
-		 *
-		 * @param boolean  $admin_integration Whether to enable admin integration for TEC
-		 * @param WP_Query $query             Current WP_Query object
-		 */
-		$admin_integration = apply_filters( 'tribe_event_elasticpress_admin', false, $query );
+		// Check if auto integration is enabled
+		$integration       = Tribe__Events__Elasticsearch__Main::get_option( 'enable_ep_integrate', true );
+		$admin_integration = Tribe__Events__Elasticsearch__Main::get_option( 'enable_ep_integrate_admin', false );
 
-		if ( is_admin() && true !== $admin_integration ) {
+		if ( true !== $integration ) {
+			// Auto integration is not enabled
+			return;
+		} elseif ( is_admin() && true !== $admin_integration ) {
+			// Auto integration for admin is not enabled
 			return;
 		}
 
@@ -170,6 +176,244 @@ class Tribe__Events__Elasticsearch__ElasticPress {
 		}
 
 		return $is_whitelisted;
+
+	}
+
+	/**
+	 * Add ElasticPress integration for TEC queries.
+	 *
+	 * @param WP_Query $query Query object
+	 */
+	public function add_ep_query_integration( $query ) {
+
+		// Convert our WHERE SQL logic to WP_Query for ElasticPress to use
+		$this->add_ep_query_integration_where( $query );
+
+		// Convert our ORDER BY SQL logic to WP_Query for ElasticPress to use
+		$this->add_ep_query_integration_orderby( $query );
+
+	}
+
+	/**
+	 * Remove TEC wpdb overrides for WP_Query.
+	 *
+	 * @param WP_Query $query Query object
+	 */
+	public function remove_tec_wpdb_overrides( $query ) {
+
+		// Skip our logic if EP isn't integrated with this query
+		if ( false === $query->get( 'ep_integrate', false ) ) {
+			return;
+		}
+
+		remove_filter( 'posts_fields', array( 'Tribe__Events__Query', 'posts_fields' ) );
+		remove_filter( 'posts_fields', array( 'Tribe__Events__Query', 'multi_type_posts_fields' ) );
+		remove_filter( 'posts_join', array( 'Tribe__Events__Query', 'posts_join' ) );
+		remove_filter( 'posts_join', array( 'Tribe__Events__Query', 'posts_join_venue_organizer' ) );
+
+		remove_filter( 'posts_where', array( 'Tribe__Events__Query', 'posts_where' ) );
+
+		remove_filter( 'posts_orderby', array( 'Tribe__Events__Query', 'posts_orderby' ) );
+
+	}
+
+	/**
+	 * Add ElasticPress 'where' integration for TEC queries.
+	 *
+	 * @param WP_Query $query Query object
+	 */
+	public function add_ep_query_integration_where( $query ) {
+
+		$dates = $this->get_tec_dates_from_query( $query );
+
+		$start_date = $dates['start'];
+		$end_date   = $dates['end'];
+
+		$meta_query = $query->get( 'meta_query', array() );
+
+		if ( ! is_array( $meta_query ) ) {
+			$meta_query = array();
+		}
+
+		if ( '' !== $start_date || ! empty( $meta_query['relation'] ) && 'OR' === strtoupper( $meta_query['relation'] ) ) {
+			// EP doesn't do date ranges with date_query well for our purposes (nested)
+			// We can't do nested 'relation' since this filtering needs to be 'AND' so it adds to main query
+			// The below hook above also handles cases like: '' !== $start_date && '' !== $end_date
+
+			// We handle date ranges in EP API args handler
+			add_action( 'ep_formatted_args', array( $this, 'add_ep_api_integration_where' ), 10, 2 );
+		} elseif ( '' !== $end_date ) {
+			// Handle using a simple meta_query arg
+			$meta_query[] = array(
+				'key'     => '_EventStartDate',
+				'value'   => $end_date,
+				'compare' => '<=', // wp_postmeta.meta_value <= $value
+				'type'    => 'DATE',
+			);
+		}
+
+	}
+
+	/**
+	 * Add ElasticPress 'orderby' integration for TEC queries.
+	 *
+	 * @param WP_Query $query Query object
+	 */
+	public function add_ep_query_integration_orderby( $query ) {
+
+		$orderby = $query->get( 'orderby' );
+		$order   = $query->get( 'order' );
+
+		// We store _EventStartDate as post_date, we filter data being sent to Elasticsearch to make adjustments
+
+		if ( 'event_date' === $orderby ) {
+			$query->set( 'orderby', 'date' );
+		} elseif ( in_array( $orderby, array( 'title', 'menu_order' ), true ) ) {
+			$query->set( 'orderby', $orderby . ' date' );
+		} elseif ( 'venue' === $orderby ) {
+			/**
+			 * We need to be able to sort by venue.post_title here
+			 * but Elasticsearch can't do that without a custom ES script.
+			 *
+			 * We could store venue title in Elasticsearch post meta
+			 * being indexed and update it any time someone updates/deletes
+			 * a venue that matches (by ID).
+			 *
+			 * There aren't presently any great options for this, they are
+			 * all hacks unfortunately.
+			 */
+		} elseif ( 'organizer' === $orderby ) {
+			/**
+			 * We need to be able to sort by organizer.post_title here
+			 * but Elasticsearch can't do that without a custom ES script.
+			 *
+			 * We could store organizer title in Elasticsearch post meta
+			 * being indexed and update it any time someone updates/deletes
+			 * an organizer that matches (by ID).
+			 *
+			 * There aren't presently any great options for this, they are
+			 * all hacks unfortunately.
+			 */
+		}
+
+	}
+
+	/**
+	 * Add Elasticsearch 'where' integration for TEC queries.
+	 *
+	 * @param array $formatted_args Elasticsearch formatted args
+	 * @param array $args           WP_Query args
+	 */
+	public function add_ep_api_integration_where( $formatted_args, $args ) {
+
+		$dates = $this->get_tec_dates_from_query( $query );
+
+		$start_date = $dates['start'];
+		$end_date   = $dates['end'];
+
+		if ( '' !== $start_date && '' !== $end_date ) {
+			// Handle full date range
+
+			// @todo Setup EP date query handler to create args
+
+			// OLD SQL
+			$start_clause  = $wpdb->prepare( "( %s <= $event_start_date AND $event_start_date <= %s )", $start_date, $end_date );
+			// OR THIS
+			$end_clause    = $wpdb->prepare( "( %s <= $event_end_date AND $event_start_date <= %s )", $start_date, $end_date );
+			// OR THIS
+			$within_clause = $wpdb->prepare( "( $event_start_date < %s AND %s <= $event_end_date )", $start_date, $end_date );
+		} elseif ( '' !== $start_date ) {
+			// Handle partial date range
+
+			// @todo Setup EP date query handler to create args
+
+			// OLD SQL
+			$start_clause  = $wpdb->prepare( "%s <= $event_start_date", $start_date );
+			// OR this
+			$within_clause = $wpdb->prepare( "( $event_start_date <= %s AND %s <= $event_end_date )", $start_date, $start_date );
+
+			if ( $query->is_singular() && $query->get( 'eventDate' ) ) {
+				// AND this
+				$tomorrow        = date( 'Y-m-d', strtotime( $query->get( 'eventDate' ) . ' +1 day' ) );
+				$tomorrow_clause = $wpdb->prepare( "$event_start_date < %s", $tomorrow );
+			}
+		} elseif ( '' !== $end_date ) {
+			// See Tribe__Events__Elasticsearch__ElasticPress::add_ep_query_integration_where
+		}
+
+	}
+
+	/**
+	 * Get the TEC start and end dates from the WP_Query object.
+	 *
+	 * @param WP_Query $query Query object
+	 *
+	 * @return array
+	 */
+	public function get_tec_dates_from_query( $query ) {
+
+		$start_date = '';
+		$end_date   = '';
+
+		// Code below taken from Tribe__Events_Query::posts_where()
+
+		// if it's a true event query then we to setup where conditions
+		// but only if we aren't grabbing a specific post
+		if (
+			(
+				$query->tribe_is_event
+				|| $query->tribe_is_event_category
+			)
+			&& empty( $query->query_vars['name'] )
+			&& empty( $query->query_vars['p'] )
+		) {
+			$start_date = $query->get( 'start_date' );
+			$end_date   = $query->get( 'end_date' );
+			$use_utc    = Tribe__Events__Timezones::is_mode( 'site' );
+			$site_tz    = $use_utc ? Tribe__Events__Timezones::wp_timezone_string() : null;
+
+			// Sitewide timezone mode: convert the start date - if set - to UTC
+			if ( $use_utc && ! empty( $start_date ) ) {
+				$start_date = Tribe__Events__Timezones::to_utc( $start_date, $site_tz );
+			}
+
+			// Sitewide timezone mode: convert the end date - if set - to UTC
+			if ( $use_utc && ! empty( $end_date ) ) {
+				$end_date = Tribe__Events__Timezones::to_utc( $end_date, $site_tz );
+			}
+		}
+
+		$dates = array(
+			'start' => $start_date,
+			'end'   => $end_date,
+		);
+
+		return $dates;
+
+	}
+
+	/**
+	 * Override ElasticPress post_date used with _EventStartDate.
+	 *
+	 * @param array $post_args Post arguments sent to Elasticsearch for indexing
+	 * @param int   $post_id   Post ID
+	 *
+	 * @return array
+	 */
+	public function override_ep_post_date( $post_args, $post_id ) {
+
+		if ( Tribe__Events__Main::POSTTYPE === $post_args['post_type'] ) {
+			$start_date = get_post_meta( $post_id, '_EventStartDate', true );
+
+			if ( ! empty( $start_date ) ) {
+				$site_tz = $use_utc ? Tribe__Events__Timezones::wp_timezone_string() : null;
+
+				$post_args['post_date']     = $start_date;
+				$post_args['post_date_gmt'] = Tribe__Events__Timezones::to_utc( $start_date, $site_tz );
+			}
+		}
+
+		return $post_args;
 
 	}
 
